45f5c73d429212e652a2f0f0ecbdc122de2eec06
marcosc11195 <eteaac@gmail.com>
2026-02-04 20:08:01 +0100

    responsive fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './theme.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
